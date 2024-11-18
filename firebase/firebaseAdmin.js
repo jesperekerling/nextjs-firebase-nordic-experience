640f1addf72eca06706ef86a33786648228e9210
Jesper Ekerling <jesper@forme.se>
2024-11-18 17:42:09 +0100

    Firebase admin change

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,11 +1,25 @@
 import admin from 'firebase-admin';
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
 
-if (!admin.apps.length) {
-  initializeApp({
-    credential: applicationDefault(),
-  });
+try {
+  if (!admin.apps.length) {
+    initializeApp({
+      credential: applicationDefault(),
+    });
+  }
+} catch (error) {
+  console.error('Firebase admin initialization error', error);
 }
 
 export const auth = admin.auth();
 export const db = admin.firestore();
+
+export const getUserClaims = async (uid) => {
+  try {
+    const user = await auth.getUser(uid);
+    return user.customClaims;
+  } catch (error) {
+    console.error('Error fetching user claims:', error);
+    throw error;
+  }
+};
