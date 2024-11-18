ca1abe4728b72214275a58484dde5fb44b840920
Jesper Ekerling <jesper@forme.se>
2024-11-18 14:00:04 +0100

    Fixed path for packageList component

--- src/app/package/page.tsx
+++ src/app/package/page.tsx
@@ -1,5 +1,5 @@
 import { fetchPackages } from "../../../utils/fetchPackages";
-import PackageListClient from "./PackageListClient";
+import PackageListClient from "../package/(components)/PackageList";
 
 const PackagePage = async ({ searchParams }) => {
   const selectedCategory = searchParams.category || null;
